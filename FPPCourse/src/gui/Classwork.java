552158287c7574846e536b3460509d7df11fbf5c
610530 <610530@cs_labv29-302.cs.mum.edu>
2019-09-12 15:22:43 -0500

    Revert "homework5_to be continued"
    This reverts commit ec0d7a71d8092484d46b2bdf7456718b51fd6221.

--- FPPCourse/src/gui/Classwork.java
+++ FPPCourse/src/gui/Classwork.java
@@ -18,20 +18,8 @@ public class Classwork {
 		JButton register = new JButton("Register");
 		
 		userlabel.setBounds(10, 20, 40, 20);
-		passwordlabel.setBounds(10, 60, 100, 20);
-		userfiled.setBounds(100, 20, 120, 20);
-		passwordfield.setBounds(100, 60, 120, 20);
-		login.setBounds(10, 120, 80, 40);
-		register.setBounds(140, 120, 100, 40);
 		jpanel.add(userlabel);
-		jpanel.add(passwordlabel);
-		jpanel.add(userfiled);
-		jpanel.add(passwordfield);
-		jpanel.add(login);
-		jpanel.add(register);
 		jframe.add(jpanel);
-		jframe.setSize(300,300);
-		jframe.setResizable(false);
 		jframe.setVisible(true);
 		
 		
