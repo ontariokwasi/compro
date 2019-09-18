2e712bc4bdd66114e48301a42f7eeeeee210e43c
gakomeng <gakomeng@nalosolutions.com>
2019-09-17 19:34:42 -0500

    prob2 overriden

--- FPPCourse/src/homework5/Problem2.java
+++ FPPCourse/src/homework5/Problem2.java
@@ -1,110 +1,91 @@
 package homework5;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.*;
 
-public class Problem2 extends JFrame {
-
-	private static JFrame frame = new JFrame();
-	private static JButton redButton = new JButton();
-	private static JButton orangeButton = new JButton();
-	private static JButton lightGreenButton = new JButton();
-	private static JButton blueButton = new JButton();
-	private static JButton yellowButton = new JButton();
-	private static JButton voiletButton = new JButton();
-	private static JButton purpleButton = new JButton();
-	private static FlowLayout flowLayout = new FlowLayout();
+public class Problem2 {
 
 	public static void main(String[] args) {
-		new Problem2();
+		JFrame jframe = new JFrame("Rainbow Color Frame");
+		//JPanel panel = new JPanel();
+		jframe.setLayout(new FlowLayout(FlowLayout.LEFT));
+		//dimension
+		Dimension btndim = new Dimension(50,80);
+		//define buttons
+		JButton red = new JButton();
+		red.setBackground(Color.RED);
+		red.setBounds(0, 0, 0, 200);
+		red.setPreferredSize(btndim);
+		red.setOpaque(true);
+		red.setBorderPainted(false);
+		
+		JButton orange = new JButton();
+		orange.setBackground(Color.ORANGE);
+		orange.setPreferredSize(btndim);
+		orange.setOpaque(true);
+		orange.setBorderPainted(false);
+		
+		JButton yellow = new JButton();
+		yellow.setBackground(Color.YELLOW);
+		yellow.setPreferredSize(btndim);
+		yellow.setOpaque(true);
+		yellow.setBorderPainted(false);
+		
+		JButton green = new JButton();
+		green.setBackground(Color.GREEN);
+		green.setPreferredSize(btndim);
+		green.setOpaque(true);
+		green.setBorderPainted(false);
+		
+		JButton blue = new JButton();
+		blue.setBackground(Color.BLUE);
+		blue.setPreferredSize(btndim);
+		blue.setOpaque(true);
+		blue.setBorderPainted(false);
+		
+		JButton indigo = new JButton();
+		indigo.setBackground(new Color(120,0,255));
+		indigo.setPreferredSize(btndim);
+		indigo.setOpaque(true);
+		indigo.setBorderPainted(false);
+		
+		JButton violet = new JButton();
+		violet.setBackground(new Color(102,0,153));
+		violet.setPreferredSize(btndim);
+		violet.setOpaque(true);
+		violet.setBorderPainted(false);
+		
+		//add action listerners
+		red.addActionListener((e) -> {JOptionPane.showMessageDialog(green, "Red signifies passion,vitality,enthusiasm and security");});
+		orange.addActionListener((e) -> {JOptionPane.showMessageDialog(green, "Orange signifies sunshine,balance,joy,expression");});
+		yellow.addActionListener((e) -> {JOptionPane.showMessageDialog(green, "Yellow signifies energy,optimism and joy");});
+		green.addActionListener((e) -> {JOptionPane.showMessageDialog(green, "Green signifies life,renewal and energy");});
+		blue.addActionListener((e) -> {JOptionPane.showMessageDialog(green, "Blue signifies confidence,intelligence,faith");});
+		indigo.addActionListener((e) -> {JOptionPane.showMessageDialog(green, "Purple signifies stability and calmness");});
+		violet.addActionListener((e) -> {JOptionPane.showMessageDialog(green, "Violet signifies imagination,emotions and calming");});
+		
+		
+		//add buttons to panel
+		jframe.add(red);
+		jframe.add(orange);
+		jframe.add(yellow);
+		jframe.add(green);
+		jframe.add(blue);
+		jframe.add(indigo);
+		jframe.add(violet);
+		
+		//add panel to jframe
+		//jframe.add(panel);
+		
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setSize(400, 120);
+		jframe.setResizable(false);
+		jframe.setLocation(400, 300);
+		jframe.setVisible(true);
+
 	}
-
-	public Problem2() {
-		frame.setTitle("Rainbow Color Frame");
-		frame.setSize(410, 100);
-		frame.setLayout(flowLayout);
-		frame.setResizable(false);
-		flowLayout.setAlignment(FlowLayout.LEFT);
-		
-		Dimension buttonDim = new Dimension(50,50);
-		
-		//Custom colors
-		final Color purple = new Color(102,0,153);
-		final Color voilet    = new Color(238,130,238);
-
-		// Buttons
-		redButton.setBackground(Color.RED);
-		redButton.setPreferredSize(buttonDim);
-		frame.add(redButton);
-
-		orangeButton.setBackground(Color.ORANGE);
-		orangeButton.setPreferredSize(buttonDim);
-		frame.add(orangeButton);
-		
-		lightGreenButton.setBackground(Color.GREEN);
-		lightGreenButton.setPreferredSize(buttonDim);
-		frame.add(lightGreenButton);
-		
-		blueButton.setBackground(Color.BLUE);
-		blueButton.setPreferredSize(buttonDim);
-		frame.add(blueButton);
-		
-		yellowButton.setBackground(Color.YELLOW);
-		yellowButton.setPreferredSize(buttonDim);
-		frame.add(yellowButton);
-		
-		voiletButton.setBackground(voilet);
-		voiletButton.setPreferredSize(buttonDim);
-		frame.add(voiletButton);
-		
-		purpleButton.setBackground(purple);
-		purpleButton.setPreferredSize(buttonDim);
-		frame.add(purpleButton);
-		
-		//listeners
-		ButtonHandler buttonHandler = new ButtonHandler();
-		redButton.addActionListener(buttonHandler);
-		orangeButton.addActionListener(buttonHandler);
-		lightGreenButton.addActionListener(buttonHandler);
-		blueButton.addActionListener(buttonHandler);
-		yellowButton.addActionListener(buttonHandler);
-		voiletButton.addActionListener(buttonHandler);
-		purpleButton.addActionListener(buttonHandler);
-		
-		
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-	
-	private class ButtonHandler implements ActionListener{
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			JButton source = (JButton) e.getSource();
-			if(e.getSource() == redButton) {
-				JOptionPane.showMessageDialog(source, "Red signifies passion,vitality,enthusiasm and security");
-			}else if(e.getSource() == orangeButton) {
-				JOptionPane.showMessageDialog(source, "Orange signifies sunshine,balance,joy,expression");
-			}else if(e.getSource() == lightGreenButton) {
-				JOptionPane.showMessageDialog(source, "Green signifies life,renewal and energy");
-			}else if(e.getSource() == blueButton) {
-				JOptionPane.showMessageDialog(source, "Blue signifies confident,intelligence,faith");
-			}else if(e.getSource() == yellowButton) {
-				JOptionPane.showMessageDialog(source, "Yellow signifies energy,optimism and joy");
-			}else if(e.getSource() == voiletButton) {
-				JOptionPane.showMessageDialog(source, "Voilet signifies imagination,emotions and calming");
-			}else {
-				JOptionPane.showMessageDialog(source, "Purple signifies stability and calmness");
-			}
-			
-		}
-		
-		
-		
-	}
-	
-	
 
 }
