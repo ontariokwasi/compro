008fdab0a18dd3a7fd89307795929cb9a283a8ff
610530 <610530@cs105-02-08.cs.mum.edu>
2019-10-17 09:47:40 -0500

    hw template

---
+++ src/inclass/lesson9/Day2_prob1.java
@@ -0,0 +1,13 @@
+package inclass.lesson9;
+
+public class Day2_prob1 {
+	String a;
+	int b;
+	
+	//constructor
+	
+	//override equals
+	public void myMethod(Day2_prob1 ob) {
+		//check equality and print the result on console using this::equals method reference 
+	}
+}
