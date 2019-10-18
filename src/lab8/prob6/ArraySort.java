81809cde7ecaf9c78f22a8efd82c9f92b65f34d7
gakomeng <gakomeng@nalosolutions.com>
2019-10-18 00:22:38 -0500

    Ignores case

--- src/lab8/prob6/ArraySort.java
+++ src/lab8/prob6/ArraySort.java
@@ -1,16 +1,24 @@
 package lab8.prob6;
 
+import java.text.Collator;
 import java.util.Arrays;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class ArraySort {
 
 	public static void main(String[] args) {
-		String[] names = {"Alexis", "Tim", "Kyleen", "KRISTY"};
-		Consumer<String[]> list = Arrays::sort;
-		list.accept(names);
-		System.out.println(Arrays.toString(names));
+		String[] names = { "Alexis", "Tim", "Kyleen", "KRISTY" };
+		// Does not ignore case(Case Sensitive)
+		//Consumer<String[]> list = Arrays::sort;
+		//list.accept(names);
+		// System.out.println(Arrays.toString(names));
+
 		
+		// Ignores case (Case insensitive)
+		BiConsumer<String[], Collator> ls = Arrays::sort;
+		ls.accept(names, Collator.getInstance());
+		System.out.println(Arrays.toString(names));
 
 	}
 
