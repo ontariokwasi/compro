a116bf787ecbd0f56dc84fc4f9dde6b90c8fd503
gakomeng <gakomeng@nalosolutions.com>
2019-10-22 23:11:50 -0500

    homework10_3

--- src/lab10/prob3/Main.java
+++ src/lab10/prob3/Main.java
@@ -5,39 +5,28 @@ import java.util.function.*;
 
 public class Main {
 	//could return list.contains(s), but this does not generalize
-
-	public static <T> boolean contains(List<? extends T> list,T s,BiPredicate<? super T,? super T> predicate){
-		for(Object x: list) {
-			if(x == null && s == null) return true;
-			if(s == null || x == null) continue;
-		//	if(x.equals(s)) return true;
-			if(predicate.test((T) x,s)) return true;
-		}
-		return false;
-	}
-	
-	public static boolean contains1(List<String> list, String s) {
-		for(String x: list) {
+	public static <T> boolean contains(List<T> list, T s) {
+		for(T x: list) {
 			if(x == null && s == null) return true;
 			if(s == null || x == null) continue;
 			if(x.equals(s)) return true;
 		}
 		return false;	
+		
 	}
 	
-	public static boolean contains2(List<Employee> list, Employee e, 
-			BiPredicate<Employee,Employee> pred2) {
-		for(Employee emp: list) {
-			if(emp == null && e == null) return true;
-			if(emp == null || e == null) continue;
-			if(pred2.test(emp, e)) return true;
+	public static <T> boolean contains(List<? extends T> list, T e, BiPredicate<? super T, ? super T> pred) {
+		for(T item: list) {
+			if(item == null && e == null) return true;
+			if(item == null || e == null) continue;
+			if(pred.test(item, e)) return true;
 		}
 		return false;	
 	}
 	
 	public static void test1() {
 		List<String> list = Arrays.asList("Bob", "Joe", "Tom");
-		boolean result = Main.contains(list, "Tom",(s1,s2)->s1.equals(s2));
+		boolean result = Main.contains(list, "Tom");
 		System.out.println(result);
 	}
 	
@@ -58,9 +47,9 @@ public class Main {
 		list.add(new Manager(1001, "Joe", 50000, 500));
 		
 		Manager m = new Manager(1001, "Joe", 50000, 500);
-		boolean foundIt = Main.contains(list, m,
+		boolean foundIt = Main.contains(list, m, 
 				(Employee e1, Employee e2) -> e1.getId()==e2.getId());
-		System.out.println(foundIt);
+		System.out.println(foundIt);		
 	}
 	
 	public static void test4() {
@@ -70,9 +59,9 @@ public class Main {
 		list.add(new Manager(1001, "Joe", 50000, 500));
 		
 		Manager m = new Manager(1001, "Joe", 50000, 500);
-		boolean foundIt = Main.contains(list, m,
+		boolean foundIt = Main.contains(list, m, 
 			(Employee e, Person p) -> e.getName().equals(p.getName()));
-		System.out.println(foundIt);
+		System.out.println(foundIt);		
 	}
 	
 	public static void test5() {
@@ -82,13 +71,13 @@ public class Main {
 		list.add(new CheckingAccount(1003, 125.00));
 		
 		Account a = new CheckingAccount(1002, 35.00);
-		boolean foundIt = Main.contains(list, a,
+		boolean foundIt = Main.contains(list, a, 
 			(Account a1, Account a2) -> a1.getAcctId()==a2.getAcctId());
-		System.out.println(foundIt);
+		System.out.println(foundIt);		
 	}
-
-
-
+	
+	
+	
 	public static void main(String[] args) {
 		test1();
 		test2();
